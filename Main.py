d50ce9cf2aef453a974bbbc9649513a7ee533e21
nick-vas <n.vassilikiotis@gmail.com>
2021-02-03 18:11:48 +0200

    Comments and print fixes

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
 class Vector(object):
+	#Creates a vector based on input of coordinates
     def __init__(self, coordinates):
         try:
             if not coordinates:
                 raise ValueError
             self.coordinates = tuple(coordinates)
+			#Sets the dimension of space the vector lives in
             self.dimension = len(coordinates)
 
         except ValueError:
@@ -12,10 +14,20 @@ class Vector(object):
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
-
+	#prints the coordinates of the vector 
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
 
-
+	#Compare 2 vectors and return bool of their equality 
     def __eq__(self, v):
+		#Compares amount of change
         return self.coordinates == v.coordinates
+
+#Tests
+my_vector = Vector([1,2,3])
+print (my_vector)
+my_vector2 = Vector([1,2,3])
+my_vector3 = Vector([-1,2,3])
+
+print (my_vector == my_vector2)
+print (my_vector == my_vector3)
